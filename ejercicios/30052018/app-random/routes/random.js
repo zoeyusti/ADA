5e8ef3103533f5ae617fd7446774f5f9afc539b2
Zoe <zoeyusti@gmail.com>
2018-05-30 11:52:45 -0300

    seguimos con express y pug

---
+++ ejercicios/30052018/app-random/routes/random.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET random number. */
+router.get('/', function(req, res, next) {
+
+  let number = random();
+
+  res.render('random', { title: 'The winner is', number: number});
+});
+
+
+/* GET random number with custom limit. */
+router.get('/:limit', function(req, res, next) {
+
+	if(isNaN(Number(req.params.limit))){
+
+	  res.render('random', { title: 'The winner is', number: null,error: 'Ingrese un numero'});
+
+	}else{
+		let number = randomCustom(req.params.limit);
+
+		res.render('random', { title: 'The winner is', number: number, error:null});
+	}
+
+
+ /* let number = randomCustom(req.params.limit);
+
+  res.render('random', { title: 'The winner is', number: number});*/
+});
+
+// Random de 0 a 10
+
+function random(){
+
+	return Math.floor((Math.random() * 10));
+
+}
+
+// Random de 0 al limite pasado por parametro
+
+function randomCustom(limit){
+
+	return Math.floor((Math.random() * limit));
+
+}
+
+
+
+module.exports = router;
